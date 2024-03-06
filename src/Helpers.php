94cff346c98f0dcad60cb2e16d3775ee072811c3
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-03-06 17:09:22 +0800

    added spp

--- src/Helpers.php
+++ src/Helpers.php
@@ -5,14 +5,18 @@ declare(strict_types=1);
 namespace BeycanPress\CryptoPay\Integrator;
 
 use BeycanPress\CryptoPay\Loader;
+use BeycanPress\CryptoPay\Payment;
 use BeycanPress\CryptoPay\PluginHero\Hook;
 use BeycanPress\CryptoPay\Helpers as ProHelpers;
 use BeycanPress\CryptoPay\Pages\TransactionPage;
+use BeycanPress\CryptoPay\Types\Order\OrderType;
 use BeycanPress\CryptoPay\PluginHero\Http\Response;
 // Lite
 use BeycanPress\CryptoPayLite\Loader as LiteLoader;
+use BeycanPress\CryptoPayLite\Payment as LitePayment;
 use BeycanPress\CryptoPayLite\Helpers as LiteHelpers;
 use BeycanPress\CryptoPayLite\PluginHero\Hook as LiteHook;
+use BeycanPress\CryptoPayLite\Types\Order\OrderType as LiteOrderType;
 use BeycanPress\CryptoPayLite\PluginHero\Http\Response as LiteResponse;
 use BeycanPress\CryptoPayLite\Pages\TransactionPage as LiteTransactionPage;
 
@@ -135,5 +139,82 @@ class Helpers
         } else {
             return LiteResponse::$method(...$args);
         }
+    }
+
+    /**
+     * @param array<mixed> $data
+     * @return string
+     * @throws \Exception
+     */
+    public static function createSPP(array $data): string
+    {
+        if (!isset($data['addon'])) {
+            throw new \Exception('Addon is required');
+        }
+
+        if (!isset($data['order'])) {
+            throw new \Exception('Order is required');
+        }
+
+        if (!isset($data['order']['amount'])) {
+            throw new \Exception('Order amount is required');
+        }
+
+        if (!isset($data['order']['currency'])) {
+            throw new \Exception('Order currency is required');
+        }
+
+        if (!isset($data['type'])) {
+            throw new \Exception('CryptoPay type is required');
+        }
+
+        if (!($data['type'] instanceof Type)) {
+            throw new \Exception('Invalid CryptoPay type');
+        }
+
+        $token = md5(json_encode($data) . time());
+
+        Session::set($token, $data);
+
+        return home_url("/?cp_spp={$token}");
+    }
+
+    /**
+     * @return void
+     */
+    public static function listenSPP(): void
+    {
+        $token = isset($_GET['cp_spp']) ? sanitize_text_field($_GET['cp_spp']) : null;
+
+        if ($token) {
+            extract(Session::get($token));
+            if (Type::PRO === $type) {
+                $cryptopay = self::createProPayment($addon, (array) $order);
+            } else {
+                $cryptopay = self::createLitePayment($addon, (array) $order);
+            }
+            require dirname(__DIR__) . '/views/pay.php';
+            exit;
+        }
+    }
+
+    /**
+     * @param string $addon
+     * @param array<mixed> $order
+     * @return string
+     */
+    public static function createProPayment(string $addon, array $order): string
+    {
+        return (new Payment($addon))->setOrder(OrderType::fromArray($order))->html(loading:true);
+    }
+
+    /**
+     * @param string $addon
+     * @param array<mixed> $order
+     * @return string
+     */
+    public static function createLitePayment(string $addon, array $order): string
+    {
+        return (new LitePayment($addon))->setOrder(LiteOrderType::fromArray($order))->html(loading:true);
     }
 }
